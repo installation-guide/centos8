07987b0f2dbb9303c2e23f11a3ebac30fb603bc3
phuongdvk47 <phuongdvk47@yahoo.com>
2021-07-31 23:28:52 +0700

    script install sdk

--- scripts/centos8-install-sdk.sh
+++ scripts/centos8-install-sdk.sh
@@ -48,20 +48,67 @@ while IFS= read -r line; do
     sdk_version=${fields[1]}
     case $sdk_name in
       $SDK_GCC)
+        sudo dnf install -y gcc gcc-c++ cmake
         ;;
       $SDK_OPEN_JDK)
+        export SETUP_PATH=$HOME/setups; \
+        PACKAGE_VERSION=$sdk_version; \
+        PACKAGE_NAME="openjdk-${PACKAGE_VERSION}_linux-x64_bin.tar.gz"; \
+        PACKAGE_PATH=$SETUP_PATH/$PACKAGE_NAME; \
+        PACKAGE_URL="https://download.java.net/java/GA/jdk${PACKAGE_VERSION}/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-${PACKAGE_VERSION}_linux-x64_bin.tar.gz"; \
+        TEMP="/tmp/jdk-${PACKAGE_VERSION}"; \
+        OUTPUT="/opt/open-jdk-${PACKAGE_VERSION}"; \
+        COMMAND_TYPE="tar-extract"; \
+        $SETUP_PATH/centos8-install-url.sh $PACKAGE_NAME $PACKAGE_PATH $PACKAGE_URL $TEMP $OUTPUT $COMMAND_TYPE
         ;;
       $SDK_ORACLE_JDK)
+        export SETUP_PATH=$HOME/setups; \
+        PACKAGE_VERSION=$sdk_version; \
+        PACKAGE_NAME="jdk-${PACKAGE_VERSION}_linux-x64_bin.tar.gz"; \
+        PACKAGE_PATH=$SETUP_PATH/$PACKAGE_NAME; \
+        PACKAGE_URL="null"; \
+        TEMP="/tmp/jdk-${PACKAGE_VERSION}"; \
+        OUTPUT="/opt/oracle-jdk-${PACKAGE_VERSION}"; \
+        COMMAND_TYPE="tar-extract"; \
+        $SETUP_PATH/centos8-install-url.sh $PACKAGE_NAME $PACKAGE_PATH $PACKAGE_URL $TEMP $OUTPUT $COMMAND_TYPE
+
         ;;
       $SDK_VERTX)
+        export SETUP_PATH=$HOME/setups; \
+        PACKAGE_VERSION=$sdk_version; \
+        PACKAGE_NAME="vertx-stack-manager-${PACKAGE_VERSION}-full.tar.gz"; \
+        PACKAGE_PATH=$SETUP_PATH/$PACKAGE_NAME; \
+        PACKAGE_URL="https://repo1.maven.org/maven2/io/vertx/vertx-stack-manager/${PACKAGE_VERSION}/vertx-stack-manager-${PACKAGE_VERSION}-full.tar.gz"; \
+        TEMP="/tmp/vertx"; \
+        OUTPUT="/opt/vertx-${PACKAGE_VERSION}"; \
+        COMMAND_TYPE="tar-extract"; \
+        $SETUP_PATH/centos8-install-url.sh $PACKAGE_NAME $PACKAGE_PATH $PACKAGE_URL $TEMP $OUTPUT $COMMAND_TYPE
         ;;
       $SDK_GOLANG)
+        export SETUP_PATH=$HOME/setups; \
+        PACKAGE_VERSION=$sdk_version; \
+        PACKAGE_NAME="go${PACKAGE_VERSION}.linux-amd64.tar.gz"; \
+        PACKAGE_PATH=$SETUP_PATH/$PACKAGE_NAME; \
+        PACKAGE_URL="https://golang.org/dl/$PACKAGE_NAME"; \
+        TEMP="/tmp/go"; \
+        OUTPUT="/opt/go/sdk-${PACKAGE_VERSION}"; \
+        COMMAND_TYPE="tar-extract"; \
+        $SETUP_PATH/centos8-install-url.sh $PACKAGE_NAME $PACKAGE_PATH $PACKAGE_URL $TEMP $OUTPUT $COMMAND_TYPE
         ;;
       $SDK_NODEJS)
+        export SETUP_PATH=$HOME/setups; \
+        PACKAGE_VERSION=$sdk_version; \
+        PACKAGE_NAME="node-${PACKAGE_VERSION}-linux-x64.tar.gz"; \
+        PACKAGE_PATH=$SETUP_PATH/$PACKAGE_NAME; \
+        PACKAGE_URL="https://nodejs.org/dist/${PACKAGE_VERSION}/$PACKAGE_NAME"; \
+        TEMP="/tmp/node-${PACKAGE_VERSION}-linux-x64"; \
+        OUTPUT="/opt/node/nodejs-${PACKAGE_VERSION}"; \
+        COMMAND_TYPE="tar-extract"; \
+        $SETUP_PATH/centos8-install-url.sh $PACKAGE_NAME $PACKAGE_PATH $PACKAGE_URL $TEMP $OUTPUT $COMMAND_TYPE
         ;;
       $SDK_MINICONDA3)
         SETUP_PATH=$HOME/setups; \
-        PACKAGE_VERSION="py39_4.10.3"; \
+        PACKAGE_VERSION=$sdk_version; \
         PACKAGE_NAME="Miniconda3-${PACKAGE_VERSION}-Linux-x86_64.sh"; \
         PACKAGE_PATH=$SETUP_PATH/$PACKAGE_NAME; \
         PACKAGE_URL="https://repo.anaconda.com/miniconda/$PACKAGE_NAME"; \
