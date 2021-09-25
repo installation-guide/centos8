02005d168c7811e754d2b600beea5840c5c05cbf
phuongdvk47 <phuongdvk47@yahoo.com>
2021-09-25 16:01:16 +0700

    Add network tools

--- scripts/centos8-yum-basic.sh
+++ scripts/centos8-yum-basic.sh
@@ -20,5 +20,5 @@ sudo dnf install -y kernel-headers kernel-devel systemd-devel
 sudo dnf install -y libmpc mpfr gmp mpfr-devel gmp-devel expat-devel gdbm-devel ncurses-devel tk-devel xz-devel
 sudo dnf install -y readline readline-devel openssl-devel libnl3-devel net-snmp-devel ipset-libs ipvsadm pcre-devel logrotate sqlite-devel
 sudo dnf install -y tar zlib zlib-devel zip unzip bzip2-devel wget telnet sysstat psmisc tcpdump lsof socat net-tools
-sudo dnf install -y git htop lm_sensors vim-common
-sudo dnf install -y python2 python3
+sudo dnf install -y git lm_sensors vim-common
+sudo dnf install -y python2 python3 htop bmon vnstat iptraf
